fd0cefc73afc55dd7478a21e070fed284c07649f
Archana K B <84127763+arch0nline@users.noreply.github.com>
2022-04-29 15:51:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 
-let characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*-+/"
-let passwordLength = 8;
+let input, showInput, showLength, showVolume, showMass
+
+window.onload = function() {
+  document.getElementById("input-el").focus();
+};
 
 
-function generatePassword(){
+function convertAll(){
+    input = document.getElementById("input-el").value
+   
+    showLength = document.getElementById("show-length")
+    showVolume = document.getElementById("show-volume")
+    showMass = document.getElementById("show-mass")
     
+    showLength.textContent = input + " feet" + " = " + (input * 3.28).toFixed(3) + " metres" 
+                    + " | " + input + " metres " + " = " + (input / 3.28).toFixed(3) + " feet"
+                        
+    showVolume.textContent = input + " litres" + " = " + (input * 0.264).toFixed(3) + " gallons" 
+                    + " | " + input + " gallons " + " = " + (input * 3.785).toFixed(3) + " litres"
     
-    let pass="";    
-    for(let i = 0; i < passwordLength; i++){
-        pass += characters[ Math.floor(Math.random() * characters.length) ]
-    }    
-    return pass;
+    showMass.textContent = input + " kilograms" + " = " + (input * 2.204).toFixed(3) + " pounds" 
+                    + " | " + input + " pounds " + " = " + (input / 2.204).toFixed(3) + " kilograms"
+   
 }
 
-function populatePasswords() {
-    
-    passwordLength = Number(document.getElementById("passwordLength").value);
-    if (passwordLength >= 6 && passwordLength <=18) {
-        document.getElementById("outputBox1").textContent = generatePassword();
-        document.getElementById("outputBox2").textContent = generatePassword();
-        document.getElementById("outputBox3").textContent = generatePassword();
-        document.getElementById("outputBox4").textContent = generatePassword();
-    }
-    else {
-        window.alert("Password length should be between 6 and 18! ")
-    }
-    
-}
